65d401138be06fbf6fcf4d8667ca74705c4430c4
Hieu Nguyen <harrynguyen610@gmail.com>
2023-06-09 10:27:19 +0700

    first commit

---
+++ src/main/java/com/example/mycatwebsite/MyCatWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycatwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCatWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCatWebsiteApplication.class, args);
+    }
+
+}
